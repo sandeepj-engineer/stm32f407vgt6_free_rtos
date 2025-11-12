ce072256ee1a5c3c6d3ae0267fbc92b14e9807f1
sandeepj-engineer <sandeepj.engineer@gmail.com>
2025-11-12 21:34:03 +0530

    SEGGER Configs updated

--- Exercises/001_tasks/Core/Src/main.c
+++ Exercises/001_tasks/Core/Src/main.c
@@ -47,6 +47,8 @@
 
 /* USER CODE BEGIN PV */
 
+#define DWT_CTRL    (*(volatile uint32_t*)0xE0001000)
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -112,6 +114,11 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
+
+  DWT_CTRL |= (1 << 0);
+
+  SEGGER_SYSVIEW_Conf();
+  SEGGER_SYSVIEW_Start();
 
   status = xTaskCreate(task1_handler, "task_1", 200, "Hello World", 2, &task1_handle);
   configASSERT(status == pdPASS);
